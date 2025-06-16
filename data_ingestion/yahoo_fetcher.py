27337743ab81bfe4b340bba7facbd4e1f4202e3f
CaptainNemo <rohitashwachaks@gmail.com>
2025-06-16 12:44:46 -0500

    trim

--- data_ingestion/yahoo_fetcher.py
+++
@@ -1,6 +0,0 @@
-import yfinance as yf
-
-
-def fetch_yahoo_data(ticker: str, start_date: str, end_date: str):
-    data = yf.download(ticker, start=start_date, end=end_date, multi_level_index=False, threads=False)
-    return data
